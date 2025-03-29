cae68b8c727c6df4da73b99175ab23d640a87d36
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2025-03-28 21:21:05 -0300

    Me perdi no que eu tinha que fazer nesse commit

--- calc.py
+++ calc.py
@@ -2,18 +2,23 @@ class Calculadora:
     def __init__(self):
         pass
 
-    def somar(self, float: a, float: b):
+    def somar(self, a: float, b: float):
         return a + b
     
-    def subtrair(self, float: a, float: b):
+    def subtrair(self, a: float, b: float):
         return a - b
     
-    def multiplicar(self, float: a, float: b):
+    def multiplicar(self, a: float, b: float):
         return a * b
     
-    def dividir(self, float: a, float: b):
+    def dividir(self, a: float, b: float):
         if b == 0:
             print("Impossivel dividir (divisão por zero)")
             return None
         else:
             return a / b
+        
+
+c = Calculadora()
+
+print(c.somar(5, 3))  # Output: 8.0
