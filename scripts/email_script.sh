cab4735a7d43f4468a9a78c2aaf071651a186be8
GuilhermeMunizReis <guilhermemuniz1001@gmail.com>
2025-04-03 11:18:32 -0300

    Consertando notificação 14

--- scripts/email_script.sh
+++ scripts/email_script.sh
@@ -1,2 +1,10 @@
 sudo apt-get install mailutils
 mail -s "Testes executados com sucesso" $EMAIL_TO
+
+# Verifica se o e-mail foi enviado com sucesso
+if [ $? -eq 0 ]; then
+    echo "✅ E-mail enviado com sucesso!"
+else
+    echo "❌ Falha ao enviar o e-mail."
+    exit 1  # Para indicar erro no GitHub Actions
+fi
